47c14cbee41411faf959cb5e7b5f31c0b2750489
zerodegreekelvin <zerodegreekelvin@gmail.com>
2024-03-04 16:01:38 -0800

    adding dark mode theme for neutral colors and allow write to firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </React.StrictMode>
   </ChakraProvider>
